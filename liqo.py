bd4d44c9bbb289df03b0a4099f3bd5d0fc204c34
sfauzanr <sfauzanramadhan@gmail.com>
2022-12-12 07:01:07 +0700

    Test lagieess

--- liqo.py
+++ liqo.py
@@ -4,7 +4,6 @@ import itertools
 nama = ['Sami', 'Fawwaz', 'Faiz', 'Rifki', 'Azzam', 'Rijal']
 tugas = ['MC', 'Tilawah', 'Doa', 'Materi']
 jadwal = []
-# Add a comment
 
 data = random.sample(nama, len(tugas))
 jadwal.append(data)
